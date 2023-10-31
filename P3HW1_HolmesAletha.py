ef02ddbac996bdebe9c40f315b825e962597a2c1
Cityoftherose <145370086+Cityoftherose@users.noreply.github.com>
2023-10-31 18:13:25 -0400

    Add files via upload
    Debugging & correcting program

---
+++ P3HW1_HolmesAletha.py
@@ -0,0 +1,41 @@
+# Debugging a program
+# 10/31/23
+# CTI-110 P3HW1- Debug
+# Aletha Holmes
+
+# This program takes a number grade , determines average and displays letter grade for average.
+
+# Enter grades for six modules
+mod_1 = float(input("Enter grade for Module 1: "))
+mod_2 = float(input("Enter grade for Module 2: "))
+mod_3 = float(input("Enter grade for Module 3: "))
+mod_4 = float(input("Enter grade for Module 4: "))
+mod_5 = float(input("Enter grade for Module 5: "))
+mod_6 = float(input("Enter grade for Module 6: "))
+
+# add grades entered to a list
+grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
+
+# TO DO: determine lowest, highest , sum and average for grades
+low = min(grades)
+high = max(grades)
+total = sum(grades)
+avg = total/6
+
+# determine letter grade for average
+print("")
+print("---------------Results-----------")
+print(f'Lowest Grade:          {low:.1f}')
+print(f'Highest Grade:         {high:.1f}')
+print(f'Sum of Grades:         {total:.1f}')
+print(f'Average:               {avg:.2f}')
+print("-------------------------------------------")
+if avg >= 90:
+    print("Your grade is: A")
+elif avg >= 80:
+    print("Your grade is: B")
+elif avg >= 60:
+    print("Your grade is: C")
+else:
+    print("Your grade is: F")
+# TO DO: finish this
